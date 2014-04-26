a77d67f5d9e5a6d658ac29477c9f0c8877820b89
Martin Caslavsky <martin.caslavsky@gooddata.com>
2014-04-26 10:01:49 +0200

    Remove JsonNode from ReportService method signatures
    closes #11

---
+++ src/main/java/com/gooddata/report/ExecResult.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2007-2014, GoodData(R) Corporation. All rights reserved.
+ */
+package com.gooddata.report;
+
+import org.codehaus.jackson.JsonNode;
+
+import static com.gooddata.Validate.notNull;
+
+/**
+ * Executed report
+ */
+public class ExecResult {
+
+    private final JsonNode jsonNode;
+
+    ExecResult(JsonNode jsonNode) {
+        this.jsonNode = notNull(jsonNode, "jsonNode");
+    }
+
+    JsonNode getJsonNode() {
+        return jsonNode;
+    }
+}
--- src/main/java/com/gooddata/report/ReportService.java
+++ src/main/java/com/gooddata/report/ReportService.java
@@ -33,29 +33,29 @@ public class ReportService extends AbstractService {
     public String exportReport(final ReportDefinition reportDefinition, final String format) {
         notNull(reportDefinition, "reportDefinition");
         notEmpty(format, "format");
-        final JsonNode execResult = executeReport(reportDefinition.getMeta().getUri());
+        final ExecResult execResult = executeReport(reportDefinition.getMeta().getUri());
         return exportReport(execResult, format);
     }
 
-    public JsonNode executeReport(final String reportDefinitionUri) {
+    public ExecResult executeReport(final String reportDefinitionUri) {
         notEmpty(reportDefinitionUri, "reportDefinitionUri");
         final ResponseEntity<String> entity = restTemplate
                 .exchange(ReportRequest.URI, POST, new HttpEntity<>(new ReportRequest(reportDefinitionUri)),
                         String.class);
         try {
-            return mapper.readTree(entity.getBody());
+            return new ExecResult(mapper.readTree(entity.getBody()));
         } catch (IOException e) {
             throw new GoodDataException("Unable to read execution result", e);
         }
     }
 
-    public String exportReport(final JsonNode execResult, final String format) {
+    public String exportReport(final ExecResult execResult, final String format) {
         notNull(execResult, "execResult");
         notEmpty(format, "format");
         final ObjectNode root = mapper.createObjectNode();
         final ObjectNode child = mapper.createObjectNode();
 
-        child.put("result", execResult);
+        child.put("result", execResult.getJsonNode());
         child.put("format", format);
         root.put("result_req", child);
 
