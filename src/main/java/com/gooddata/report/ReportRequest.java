64f9f429c0f303f361deea4cfe3b4d23585ae2ff
Libor Rysavy <libor.rysavy@gooddata.com>
2014-06-19 18:30:12 +0200

    tune report package DTOs

--- src/main/java/com/gooddata/report/ReportRequest.java
+++ src/main/java/com/gooddata/report/ReportRequest.java
@@ -9,6 +9,10 @@ import org.codehaus.jackson.annotate.JsonTypeInfo;
 import org.codehaus.jackson.annotate.JsonTypeName;
 import org.codehaus.jackson.map.annotate.JsonSerialize;
 
+/**
+ * Report execution request.
+ * Serialization only.
+ */
 @JsonTypeInfo(include = JsonTypeInfo.As.WRAPPER_OBJECT, use = JsonTypeInfo.Id.NAME)
 @JsonTypeName("report_req")
 @JsonIgnoreProperties(ignoreUnknown = true)
