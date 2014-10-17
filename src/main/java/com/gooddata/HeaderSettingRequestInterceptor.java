43d5fd065458a16c88053dc62aeec196081e61eb
Libor Rysavy <libor.rysavy@gooddata.com>
2014-10-18 00:12:50 +0200

    add and format some javadocs

--- src/main/java/com/gooddata/HeaderSettingRequestInterceptor.java
+++ src/main/java/com/gooddata/HeaderSettingRequestInterceptor.java
@@ -14,14 +14,35 @@ import java.util.Map;
 
 import static com.gooddata.Validate.notNull;
 
+/**
+ * Intercepts client-side HTTP requests and sets HTTP headers passed to constructor of this class.
+ * Implementations of this interface can be registered with the RestTemplate, as to modify the outgoing
+ * ClientHttpRequest and/or the incoming ClientHttpResponse.
+ */
 class HeaderSettingRequestInterceptor implements ClientHttpRequestInterceptor {
 
     private final Map<String, String> headers;
 
+    /**
+     * Construct interceptor for setting given HTTP headers.
+     *
+     * @param headers the map of HTTP header names to header values
+     */
     public HeaderSettingRequestInterceptor(Map<String, String> headers) {
         this.headers = notNull(headers, "headers");
     }
 
+    /**
+     * Intercept the given request, set headers passed to constructor, and return a response.
+     * The given {@link org.springframework.http.client.ClientHttpRequestExecution} allows the interceptor
+     * to pass on the request and response to the next entity in the chain.
+     *
+     * @param request   the request, containing method, URI, and headers
+     * @param body      the body of the request
+     * @param execution the request execution
+     * @return the response
+     * @throws IOException in case of I/O errors
+     */
     @Override
     public ClientHttpResponse intercept(HttpRequest request, byte[] body,
                                         ClientHttpRequestExecution execution) throws IOException {
--- src/main/java/com/gooddata/UriPrefixingClientHttpRequestFactory.java
+++ src/main/java/com/gooddata/UriPrefixingClientHttpRequestFactory.java
@@ -14,6 +14,7 @@ import java.net.URI;
 import static com.gooddata.Validate.notNull;
 
 /**
+ * Factory for ClientHttpRequest objects.
  * Sets default URI prefix for Spring REST client which by default requires absolute URI.
  * UriPrefixingAsyncClientHttpRequestFactory allows you to specify default hostname and port.
  */
@@ -23,16 +24,26 @@ class UriPrefixingClientHttpRequestFactory implements ClientHttpRequestFactory {
     private final UriPrefixer prefixer;
 
     /**
-     * Creates na instance
+     * Create an instance that will wrap the given {@link org.springframework.http.client.ClientHttpRequestFactory}
+     * and use the given URI for setting hostname and port of all HTTP requests
      *
-     * @param factory
-     * @param uriPrefix
+     * @param factory   the factory to be wrapped
+     * @param uriPrefix the URI for setting hostname and port of all HTTP requests
      */
     UriPrefixingClientHttpRequestFactory(ClientHttpRequestFactory factory, URI uriPrefix) {
         this.wrapped = notNull(factory, "factory");
         this.prefixer = new UriPrefixer(uriPrefix);
     }
 
+    /**
+     * Create an instance that will wrap the given {@link org.springframework.http.client.ClientHttpRequestFactory}
+     * and use the given hostname, port, and protocol for all HTTP requests
+     *
+     * @param factory  the factory to be wrapped
+     * @param hostname the hostname for all HTTP requests
+     * @param port     the port for all HTTP requests
+     * @param protocol the protocol for all HTTP requests
+     */
     UriPrefixingClientHttpRequestFactory(ClientHttpRequestFactory factory, String hostname, int port, String protocol) {
         this(factory, UriComponentsBuilder.newInstance().scheme(protocol).host(hostname).port(port).build().toUri());
     }
