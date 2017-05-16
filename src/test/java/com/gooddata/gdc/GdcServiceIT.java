760dcef6dd594ac0c0ca1a5d74d5688964c00c62
Matej Plch <matej.plch@gooddata.com>
2017-05-16 12:13:56 +0200

    test: add custom user agent IT test

--- src/test/java/com/gooddata/gdc/GdcServiceIT.java
+++ src/test/java/com/gooddata/gdc/GdcServiceIT.java
@@ -10,9 +10,9 @@ import org.testng.annotations.Test;
 
 import static com.gooddata.util.ResourceUtils.readFromResource;
 import static net.jadler.Jadler.onRequest;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
-import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GdcServiceIT extends AbstractGoodDataIT {
 
@@ -31,21 +31,6 @@ public class GdcServiceIT extends AbstractGoodDataIT {
         assertThat(gdc.getUserStagingUri(), is("/uploads"));
     }
 
-    @SuppressWarnings("deprecation")
-    @Test
-    public void shouldUseProperVersionHeader() throws Exception {
-        onRequest()
-                .havingMethodEqualTo("GET")
-                .havingPathEqualTo("/gdc")
-                .havingHeaderEqualTo("Accept", "application/json;version=1")
-                .respond()
-                .withBody(readFromResource("/gdc/gdc.json"))
-                .withStatus(200);
-
-        final Gdc gdc = gd.getGdcService().getGdc();
-        assertThat(gdc, is(notNullValue()));
-    }
-
     @Test
     public void shouldReturnRootLinks() throws Exception {
         onRequest()
@@ -60,17 +45,4 @@ public class GdcServiceIT extends AbstractGoodDataIT {
         assertThat(rootLinks.getUserStagingUri(), is("/uploads"));
     }
 
-    @Test
-    public void rootLinksShouldUseProperVersionHeader() throws Exception {
-        onRequest()
-                .havingMethodEqualTo("GET")
-                .havingPathEqualTo("/gdc")
-                .havingHeaderEqualTo("Accept", "application/json;version=1")
-                .respond()
-                .withBody(readFromResource("/gdc/gdc.json"))
-                .withStatus(200);
-
-        final RootLinks rootLinks = gd.getGdcService().getRootLinks();
-        assertThat(rootLinks, is(notNullValue()));
-    }
 }
