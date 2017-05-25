ce53adb8104ee101ae9b60ab429277acce2a9ea5
Jiri Mikulasek <jiri.mikulasek@gooddata.com>
2017-05-25 14:22:48 +0200

    trivial: change license headers to plain comment style instead of javadoc one

--- src/main/java/com/gooddata/project/Roles.java
+++ src/main/java/com/gooddata/project/Roles.java
@@ -1,5 +1,5 @@
-/**
- * Copyright (C) 2004-2016, GoodData(R) Corporation. All rights reserved.
+/*
+ * Copyright (C) 2004-2017, GoodData(R) Corporation. All rights reserved.
  * This source code is licensed under the BSD-style license found in the
  * LICENSE.txt file in the root directory of this source tree.
  */
