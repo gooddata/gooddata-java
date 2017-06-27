b6def05b1d99314c61af1c1035119718d03af985
Richard Benkovsky <richard.benkovsky@gmail.com>
2017-06-27 13:05:20 +0200

    Introduce factory methods to WarehouseUser
    I've also introduced com.gooddata.warehouse.WarehouseUserRole which should be used together with these factory methods for better user experience.

---
+++ src/main/java/com/gooddata/warehouse/WarehouseUserRole.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2004-2017, GoodData(R) Corporation. All rights reserved.
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+package com.gooddata.warehouse;
+
+/**
+ * Role of the user in the Datawarehouse
+ */
+public enum WarehouseUserRole {
+    ADMIN("admin"),
+    DATA_ADMIN("dataAdmin"),
+    READ_ONLY("readOnly");
+
+    private final String roleName;
+
+    WarehouseUserRole(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+}
