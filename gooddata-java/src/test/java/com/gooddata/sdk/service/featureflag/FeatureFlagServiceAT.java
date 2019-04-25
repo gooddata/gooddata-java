fd5e4e17cb1d2f49d5619a5ce035d4462e83fc3c
MilanKovacik <milan.kovacik@gooddata.com>
2019-04-25 09:19:55 +0200

    test project feature flag change

--- gooddata-java/src/test/java/com/gooddata/sdk/service/featureflag/FeatureFlagServiceAT.java
+++ gooddata-java/src/test/java/com/gooddata/sdk/service/featureflag/FeatureFlagServiceAT.java
@@ -5,16 +5,21 @@
  */
 package com.gooddata.sdk.service.featureflag;
 
-import com.gooddata.sdk.service.AbstractGoodDataAT;
+import com.gooddata.GoodDataException;
 import com.gooddata.sdk.model.featureflag.FeatureFlag;
 import com.gooddata.sdk.model.featureflag.FeatureFlags;
 import com.gooddata.sdk.model.featureflag.ProjectFeatureFlag;
 import com.gooddata.sdk.model.featureflag.ProjectFeatureFlags;
+import com.gooddata.sdk.service.AbstractGoodDataAT;
 import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.testng.Assert.fail;
 
 /**
  * Feature flag acceptance tests.
@@ -68,14 +73,12 @@ public class FeatureFlagServiceAT extends AbstractGoodDataAT {
         // disable (update) feature flag
         featureFlag.setEnabled(false);
         final ProjectFeatureFlag disabledFlag = gd.getFeatureFlagService().updateProjectFeatureFlag(featureFlag);
-        // can not check value because of eventual consistency of this API
-        // checkProjectFeatureFlag(disabledFlag, false);
+        checkProjectFeatureFlag(disabledFlag, PROJECT_FEATURE_FLAG, false);
 
         // enable again
         featureFlag.setEnabled(true);
         final ProjectFeatureFlag enabledFlag = gd.getFeatureFlagService().updateProjectFeatureFlag(featureFlag);
-        // can not check value because of eventual consistency of this API
-        // checkProjectFeatureFlag(enabledFlag, true);
+        checkProjectFeatureFlag(enabledFlag, PROJECT_FEATURE_FLAG , true);
     }
 
     @Test(groups = "featureFlag", dependsOnMethods = "createProjectFeatureFlag")
@@ -85,7 +88,22 @@ public class FeatureFlagServiceAT extends AbstractGoodDataAT {
                         new ProjectFeatureFlag("settingName1"));
 
         gd.getFeatureFlagService().deleteProjectFeatureFlag(featureFlag);
-        // intentionally don't check the flag is really deleted because of eventual consistency of this API
+        try {
+            gd.getFeatureFlagService().getProjectFeatureFlag(project, "settingName1");
+            fail();
+        } catch (GoodDataException e) {
+            assertThat(e.getMessage(), containsString("Unable to get project feature flag:"));
+        }
+    }
+
+    @Test(groups = "featureFlag", dependsOnMethods = "updateProjectFeatureFlag")
+    public void changeProjectFeatureFlag() throws Exception {
+        final FeatureFlagService featureFlagService = gd.getFeatureFlagService();
+        ProjectFeatureFlag featureFlag = featureFlagService.getProjectFeatureFlag(project, PROJECT_FEATURE_FLAG);
+        boolean value = !featureFlag.isEnabled();
+        featureFlag.setEnabled(value);
+        featureFlagService.updateProjectFeatureFlag(featureFlag);
+        checkProjectFeatureFlag(featureFlag, PROJECT_FEATURE_FLAG, value);
     }
 
     private void checkProjectFeatureFlag(final ProjectFeatureFlag featureFlag, final String expectedName,
