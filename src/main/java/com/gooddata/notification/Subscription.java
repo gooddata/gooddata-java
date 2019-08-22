9a79ce67ff892f0d28bd2f1ededc9a6e01fcb79b
apascenko <anna.pascenko@gooddata.com>
2019-08-22 12:54:14 +0200

    Add email subject to subscription
    Currently, it's impossible to specify the subscription email subject, only field for the email body template is defined. The subject added as per https://confluence.intgdc.com/display/analysis/Notifications+-+User+documentation.

--- src/main/java/com/gooddata/notification/Subscription.java
+++ src/main/java/com/gooddata/notification/Subscription.java
@@ -5,11 +5,13 @@
  */
 package com.gooddata.notification;
 
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 import static org.apache.commons.lang3.Validate.notEmpty;
 import static org.apache.commons.lang3.Validate.notNull;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeName;
@@ -26,6 +28,7 @@ import java.util.stream.Collectors;
 @JsonTypeName("subscription")
 @JsonTypeInfo(include = JsonTypeInfo.As.WRAPPER_OBJECT, use = JsonTypeInfo.Id.NAME)
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(NON_NULL)
 public class Subscription {
 
     public static final String URI = "/gdc/projects/{project}/users/{user}/subscriptions";
@@ -33,7 +36,8 @@ public class Subscription {
 
     private final List<Trigger> triggers;
     private final TriggerCondition condition;
-    private final MessageTemplate template;
+    private final MessageTemplate messageTemplate;
+    private final MessageTemplate subjectTemplate;
     private final List<String> channels;
     private final Meta meta;
 
@@ -44,33 +48,55 @@ public class Subscription {
      * @param triggers triggers of subscription
      * @param channels list of {@link Channel}
      * @param condition condition under which this subscription activates
-     * @param template of message
+     * @param messageTemplate of message
      * @param title name of subscription
      */
     public Subscription(final List<Trigger> triggers,
                         final List<Channel> channels,
                         final TriggerCondition condition,
-                        final MessageTemplate template,
+                        final MessageTemplate messageTemplate,
+                        final String title) {
+        this(triggers, channels, condition, messageTemplate, null, title);
+    }
+
+    /**
+     * Creates Subscription
+     *
+     * @param triggers triggers of subscription
+     * @param channels list of {@link Channel}
+     * @param condition condition under which this subscription activates
+     * @param messageTemplate of message
+     * @param subjectTemplate of message
+     * @param title name of subscription
+     */
+    public Subscription(final List<Trigger> triggers,
+                        final List<Channel> channels,
+                        final TriggerCondition condition,
+                        final MessageTemplate messageTemplate,
+                        final MessageTemplate subjectTemplate,
                         final String title) {
         this(notNull(triggers, "triggers"),
-             notNull(condition, "condition"),
-             notNull(template, "template"),
-             notNull(channels, "channels").stream().map(e -> e.getMeta().getUri()).collect(Collectors.toList()),
-             new Meta(
-                     notEmpty(title, "title")
-             )
+                notNull(condition, "condition"),
+                notNull(messageTemplate, "messageTemplate"),
+                subjectTemplate,
+                notNull(channels, "channels").stream().map(e -> e.getMeta().getUri()).collect(Collectors.toList()),
+                new Meta(
+                        notEmpty(title, "title")
+                )
         );
     }
 
     @JsonCreator
     Subscription(@JsonProperty("triggers") final List<Trigger> triggers,
                  @JsonProperty("condition") final TriggerCondition condition,
-                 @JsonProperty("message") final MessageTemplate template,
+                 @JsonProperty("message") final MessageTemplate messageTemplate,
+                 @JsonProperty("subject") final MessageTemplate subjectTemplate,
                  @JsonProperty("channels") final List<String> channels,
                  @JsonProperty("meta") final Meta meta) {
         this.triggers = triggers;
         this.condition = condition;
-        this.template = template;
+        this.messageTemplate = messageTemplate;
+        this.subjectTemplate = subjectTemplate;
         this.channels = channels;
         this.meta = meta;
     }
@@ -85,7 +111,12 @@ public class Subscription {
 
     @JsonProperty("message")
     public MessageTemplate getTemplate() {
-        return template;
+        return messageTemplate;
+    }
+
+    @JsonProperty("subject")
+    public MessageTemplate getSubjectTemplate() {
+        return subjectTemplate;
     }
 
     public List<String> getChannels() {
