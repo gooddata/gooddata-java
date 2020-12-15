5f8c5db1ce85a2a6b98859e3e48db2e5c1512267
Adam Štulpa <adam.stulpa@gooddata.com>
2020-12-15 17:43:43 +0100

    Add Zendesk reload API support

---
+++ gooddata-java-model/src/main/java/com/gooddata/sdk/model/connector/Reload.java
@@ -0,0 +1,148 @@
+/*
+ * Copyright (C) 2004-2020, GoodData(R) Corporation. All rights reserved.
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+package com.gooddata.sdk.model.connector;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * Connector reload.
+ */
+@JsonTypeName("reload")
+@JsonTypeInfo(include = JsonTypeInfo.As.WRAPPER_OBJECT, use = JsonTypeInfo.Id.NAME)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Reload {
+
+    public static final String URL = "/gdc/projects/{project}/connectors/zendesk4/integration/reloads";
+
+    public static final String CHATS_START_TIME_PROPERTY = "chats";
+    public static final String AGENT_TIMELINE_START_TIME_PROPERTY = "agentTimeline";
+
+    /**
+     * Reload was scheduled but not yet started.
+     */
+    public static final String STATUS_DO = "DO";
+
+    /**
+     * Reload is running.
+     */
+    public static final String STATUS_RUNNING = "RUNNING";
+
+    /**
+     * Reload was successfully finished.
+     */
+    public static final String STATUS_FINISHED = "FINISHED";
+
+    /**
+     * Reload wasn't started because reload window was missed.
+     */
+    public static final String STATUS_MISSED = "MISSED";
+
+    /**
+     * Reload finished with error.
+     */
+    public static final String STATUS_ERROR = "ERROR";
+
+    private static final String SELF_LINK = "self";
+    private static final String PROCESS_LINK = "process";
+    private static final String INTEGRATION_LINK = "integration";
+
+
+    private final Integer id;
+
+    private final Map<String, Long> startTimes;
+
+    private final String status;
+
+    private final String processId;
+
+    private final Map<String, String> links;
+
+    public Reload(Map<String, Long> startTimes) {
+        this(null, startTimes, null, null, null);
+    }
+
+    @JsonCreator
+    public Reload(
+            @JsonProperty("id") final Integer id,
+            @JsonProperty("startTimes") final Map<String, Long> startTimes,
+            @JsonProperty("status") final String status,
+            @JsonProperty("processId") final String processId,
+            @JsonProperty("links") final Map<String, String> links) {
+        this.id = id;
+        this.startTimes = startTimes;
+        this.status = status;
+        this.processId = processId;
+        this.links = links;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Map<String, Long> getStartTimes() {
+        return startTimes;
+    }
+
+    @JsonIgnore
+    public Long getChatsStartTime() {
+        return startTimes.get(CHATS_START_TIME_PROPERTY);
+    }
+
+    @JsonIgnore
+    public Long getAgentTimelineStartTime() {
+        return startTimes.get(AGENT_TIMELINE_START_TIME_PROPERTY);
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getProcessId() {
+        return processId;
+    }
+
+    public Map<String, String> getLinks() {
+        return links;
+    }
+
+    /**
+     * @return URI to itself.
+     */
+    @JsonIgnore
+    public Optional<String> getUri() {
+        return getLink(SELF_LINK);
+    }
+
+    /**
+     * @return URI to running process. Is empty if reload wasn't started yet.
+     */
+    @JsonIgnore
+    public Optional<String> getProcessUri() {
+        return getLink(PROCESS_LINK);
+    }
+
+    /**
+     * @return URI to integration.
+     */
+    @JsonIgnore
+    public Optional<String> getIntegrationUri() {
+        return getLink(INTEGRATION_LINK);
+    }
+
+    private Optional<String> getLink(final String linkName) {
+        return links != null ? Optional.ofNullable(links.get(linkName)) : Optional.empty();
+    }
+}
---
+++ gooddata-java-model/src/test/java/com/gooddata/sdk/model/connector/ReloadTest.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (C) 2004-2019, GoodData(R) Corporation. All rights reserved.
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+package com.gooddata.sdk.model.connector;
+
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.testng.Assert.assertFalse;
+import static org.testng.AssertJUnit.assertNull;
+
+public class ReloadTest {
+
+    private static final int RELOAD_ID = 123;
+    private static final String PROCESS_ID = "processId";
+
+    private static final String SELF_LINK = "/self/link";
+    private static final String INTEGRATION_LINK = "/integration/link";
+    private static final String PROCESS_LINK = "/process/link";
+
+    private static final long CHATS_START_TIME = 1112L;
+    private static final long AGENT_TIMELINE_START_TIME = 0L;
+
+    private Reload reload;
+    private Map<String, Long> startTimes;
+    private Map<String, String> links;
+
+    @BeforeMethod
+    public void setUp() {
+        startTimes = new HashMap<>();
+        startTimes.put(Reload.CHATS_START_TIME_PROPERTY, CHATS_START_TIME);
+        startTimes.put(Reload.AGENT_TIMELINE_START_TIME_PROPERTY, AGENT_TIMELINE_START_TIME);
+
+        links = new HashMap<>();
+        links.put("self", SELF_LINK);
+        links.put("integration", INTEGRATION_LINK);
+        links.put("process", PROCESS_LINK);
+
+        reload = new Reload(RELOAD_ID, startTimes, Reload.STATUS_RUNNING, PROCESS_ID, links);
+    }
+
+    @Test
+    public void testGetId() {
+        assertThat(reload.getId(), is(RELOAD_ID));
+    }
+
+    @Test
+    public void testGetStartTimes() {
+        assertThat(reload.getStartTimes(), is(startTimes));
+    }
+
+    @Test
+    public void testGetChatsStartTime() {
+        assertThat(reload.getChatsStartTime(), is(CHATS_START_TIME));
+    }
+
+    @Test
+    public void testGetAgentTimelineStartTime() {
+        assertThat(reload.getAgentTimelineStartTime(), is(AGENT_TIMELINE_START_TIME));
+    }
+
+    @Test
+    public void testGetStatus() {
+        assertThat(reload.getStatus(), is(Reload.STATUS_RUNNING));
+    }
+
+    @Test
+    public void testGetProcessId() {
+        assertThat(reload.getProcessId(), is(PROCESS_ID));
+    }
+
+    @Test
+    public void testGetLinks() {
+        assertThat(reload.getLinks(), is(links));
+    }
+
+    @Test
+    public void testGetUri() {
+        assertThat(reload.getUri().get(), is(SELF_LINK));
+    }
+
+    @Test
+    public void testGetProcessUri() {
+        assertThat(reload.getProcessUri().get(), is(PROCESS_LINK));
+    }
+
+    @Test
+    public void testGetIntegrationUri() {
+        assertThat(reload.getIntegrationUri().get(), is(INTEGRATION_LINK));
+    }
+
+    @Test
+    public void constructor2nd() {
+        reload = new Reload(startTimes);
+        assertNull(reload.getId());
+        assertThat(reload.getStartTimes(), is(startTimes));
+        assertThat(reload.getChatsStartTime(), is(CHATS_START_TIME));
+        assertThat(reload.getAgentTimelineStartTime(), is(AGENT_TIMELINE_START_TIME));
+        assertNull(reload.getStatus());
+        assertNull(reload.getProcessId());
+        assertNull(reload.getLinks());
+        assertFalse(reload.getUri().isPresent());
+        assertFalse(reload.getProcessUri().isPresent());
+        assertFalse(reload.getIntegrationUri().isPresent());
+    }
+}
